f751986cddacee02abd4f31658ae9aaca613e641
tomcatprime <101501305+tomcatprime@users.noreply.github.com>
2024-02-16 10:40:04 +0000

    Day1 Commit

---
+++ Day1/printing-start.py
@@ -0,0 +1,10 @@
+print("This is first printing to Console")
+
+#print in new line using \n
+print("This is line1.\nThis is line 2")
+
+#combine strings
+
+print("Hello" + " TomcatPrime") #combine without space
+
+print("Hello" +" "+ "TomcatPrime") # combine with space or just simply add space in any of string.
