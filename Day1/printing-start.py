429bee0628b6cf1d8af5853cf678e77a0b491eeb
tomcatprime <101501305+tomcatprime@users.noreply.github.com>
2024-02-16 11:07:53 +0000

    Day1 commit

--- Day1/printing-start.py
+++ Day1/printing-start.py
@@ -8,3 +8,9 @@ print("This is line1.\nThis is line 2")
 print("Hello" + " TomcatPrime") #combine without space
 
 print("Hello" +" "+ "TomcatPrime") # combine with space or just simply add space in any of string.
+
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
