5834c530b43c0f7ec144409e21227eda5e62e022
tomcatprime <101501305+tomcatprime@users.noreply.github.com>
2024-02-16 11:18:07 +0000

    Day1Commit

--- Day1/printing-start.py
+++ Day1/printing-start.py
@@ -14,3 +14,11 @@ print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+
+#input
+input("What is your nickname")
+print("Hello " + input("What is your name?"))
+
+name = input("Type your name")
+print(len(name))
+
