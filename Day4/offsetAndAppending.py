81cc9b1eb22e510784d9e899a8a7749249652017
tomcatprime <101501305+tomcatprime@users.noreply.github.com>
2024-02-18 10:15:05 +0000

    Day4

--- Day4/offsetAndAppending.py
+++ Day4/offsetAndAppending.py
@@ -1,5 +1,19 @@
 # list - starts always with [] 
 # frutis = [item1, item2]
-
+# if index is minus, will start from end of list
 states = ["Delaware", "Pennsylvania", "Arizona", "Texas"]
 print(states[1])
+# if index is minus, will start from end of list
+print(states[-1])
+
+# Modify item in list
+states[2] = "Ariz"
+
+#add item to the end of the list .append()
+
+states.append("Los Angeles")
+print(states)
+
+# adding bunch of items .extend()
+states.extend(["Miami", "Florida"])
+print(states)
