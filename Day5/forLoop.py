410e0e28fb45c6acea34164b64112a5ef2a4947f
tomcatprime <101501305+tomcatprime@users.noreply.github.com>
2024-02-19 15:55:11 +0000

    Day5 commit

---
+++ Day5/forLoop.py
@@ -0,0 +1,8 @@
+# for loops
+# for item in list_of_items:    
+#    do something to each item
+
+fruits = ["Apples", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
