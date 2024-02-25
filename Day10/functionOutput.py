58ca6411c6c92d2c303c05043eb49b770160a0c8
tomcatprime <karolchru@gmail.com>
2024-02-25 11:08:44 +0000

    Day10 commit

--- Day10/functionOutput.py
+++ Day10/functionOutput.py
@@ -1,8 +1,11 @@
 # Functions with output
 a = int(input("Type number: "))
 b = int(input("Type number: "))
+
+
 def my_function(a, b):
     return a * b
+
 
 output = my_function(a, b)
 
@@ -13,13 +16,18 @@ lastName = input("Type nyour last name: ")
 
 
 def formatName(f_name, l_name):
-    if f_name = "" or l_name = ""
+    # docstring
+    """Take a first and last name and format it to return the title case version of the name."""
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
 
     return f"{formated_f_name} {formated_l_name}"
 
+
 formated_string = formatName(name, lastName)
 
 print(formated_string)
 
+formatName()
