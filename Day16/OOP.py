bb16913cd6f153308cc269f49aadb58e547b4ab6
tomcatprime <karolchru@gmail.com>
2024-03-01 11:01:37 +0200

    Day16 OOP coffee machine

--- Day16/OOP.py
+++ Day16/OOP.py
@@ -1,4 +1,22 @@
 #Object Oriented Programming
-Print("Day16")
+print("Day16")
 print("Object Oriented Programming")
 
+# class writen always with Capitalized letter.
+
+
+import turtle
+import module
+
+print(module.another_variable)
+timmy = turtle.Turtle() #importing Class Turtle()
+print(timmy)
+turtle.title("This is day 16 OOP")
+timmy.shape("turtle")
+timmy.color("blue1")
+timmy.speed(1)
+timmy.forward(100)
+
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
---
+++ Day16/pypi.py
@@ -0,0 +1,9 @@
+# Day16 - OOP - importing modules
+
+from prettytable import PrettyTable
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
