c8990793ddb5c35f25ce3b337a66c6e3651d4216
tomcatprime <karolchru@gmail.com>
2024-03-09 15:30:22 +0200

    add OOP to the pong game, commit full code

--- Day22/Pong Game/main.py
+++ Day22/Pong Game/main.py
@@ -1,1 +1,55 @@
 #The Pong Game 2024 - Python
+
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=800)
+screen.title("Pong")
+screen.tracer(0)
+
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_si_on = True
+while game_si_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+    
+    #detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        #needs to bounce
+        ball.bounce_y()
+        
+    #detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -32:
+        ball.bounce_x()
+                
+    #detect r_paddle misses
+    
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+        
+    #detect l_paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+    
+    
+screen.exitonclick()
