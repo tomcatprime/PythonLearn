b488819bb2572bc10434019d979555ac42ee7330
tomcatprime <karolchru@gmail.com>
2024-03-17 12:18:29 +0200

    Day25  -U.S. Game states initial project

---
+++ Day25/US_States_quiz/main.py
@@ -0,0 +1,21 @@
+import pandas
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+#take coordinates of x, y of the states
+#def get_mouse_click_coor(x, y):
+ #   print(x, y)
+
+#turtle.onscreenclick(get_mouse_click_coor)
+#turtle.mainloop()
+
+answer_state = screen.textinput(title="Guess the State", prompt="What's anothe state's name?")
+
+print(answer_state)
+screen.exitonclick()
