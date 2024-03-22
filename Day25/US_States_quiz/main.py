1b24464f7e0d82ce59caf9da909ed4b0bc9ba25a
tomcatprime <karolchru@gmail.com>
2024-03-22 12:01:27 +0200

    finish code from day 25

--- Day25/US_States_quiz/main.py
+++ Day25/US_States_quiz/main.py
@@ -8,14 +8,36 @@ screen.addshape(image)
 
 turtle.shape(image)
 
-#take coordinates of x, y of the states
-#def get_mouse_click_coor(x, y):
- #   print(x, y)
+# take coordinates of x, y of the states
+# def get_mouse_click_coor(x, y):
+#   print(x, y)
 
-#turtle.onscreenclick(get_mouse_click_coor)
-#turtle.mainloop()
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's anothe state's name?")
 
-print(answer_state)
-screen.exitonclick()
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_stated = []
+while len(guessed_stated) < 50:
+
+    answer_state = screen.textinput(title=f"{len(guessed_stated)}/50 States Correct", prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_stated:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_lear.csv")
+        break
+
+    if answer_state in all_states:
+        guessed_stated.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+
+
