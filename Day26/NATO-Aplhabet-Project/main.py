9b700f23024fafc3ce22e2bf7da913183e728b69
tomcatprime <101501305+tomcatprime@users.noreply.github.com>
2024-03-28 11:30:59 +0000

    Nato alphabet project using dict comprehension

--- Day26/NATO-Aplhabet-Project/main.py
+++ Day26/NATO-Aplhabet-Project/main.py
@@ -9,21 +9,23 @@ for (key, value) in student_dict.items():
     pass
 
 import pandas as pd
-student_data_frame = pd.read_csv('nato_phonetic_alphabet.csv')
+data = pd.read_csv('./nato_phonetic_alphabet.csv')
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    print(row.code)
-    #Access row.student or row.score
-    pass
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     print(row.letter)
+#     #Access row.student or row.score
+#     pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 #TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+dict = {row.letter:row.code for (index,row) in data.iterrows()}
+print(dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
-# kk
+word = input("Enter a word: ").upper()
+data = [dict[letter] for letter in word]
+print(data)
