863810b86fc69f7f122f7793a5859d69ff760b28
tomcatprime <101501305+tomcatprime@users.noreply.github.com>
2024-03-29 15:36:53 +0000

    day27 commit

--- Day27 - Tkinter/main.py
+++ Day27 - Tkinter/main.py
@@ -9,30 +9,23 @@ my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
 
 my_label.pack()
 
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+#button
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+button = tkinter.Button(text="Click me!", command=button_clicked)
+button.pack()
+
+#Entry componenet - input
+
+input = tkinter.Entry(width=10)
+input.pack()
+input.get()
+
 window.mainloop()
 
-def add(*args): #*args - unlimited arguments
-    sum = 0
-    for n in args:
-        sum += n
-    return sum
-
-print(add(3, 4, 5, 6, 7, 8, 431))
-
-#**kwargs - unlimited key word arguments
-def calculate(**kwargs):
-    print(kwargs)
-    for key, value in kwargs.items():
-        print(key)
-        print(value)
-    print(kwargs["add"])
-calculate(add=3, multiply=5)
-
-
-class Car:
-    def __init__(self, **kw):
-        self.make = kw["make"]
-        self.model = kw["model"]
-
-my_Car = Car(make="Nissan", model="almera")
-print(my_Car.model)
