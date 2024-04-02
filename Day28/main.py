71b68f9468e0cad78a19ad0377dafea4d8cb6396
tomcatprime <karolchru@gmail.com>
2024-04-02 12:32:50 +0300

    finish app day 28

--- Day28/main.py
+++ Day28/main.py
@@ -10,11 +10,40 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 text="✔"
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+   global timer
+   window.after_cancel(timer)
+   canvas.itemconfig(timer_text, text="00:00")
+   title_label.config(text="Timer")
+   check_marks.config(text="")
+   reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
+   global reps
+   reps +=1
+
+   work_sec  =WORK_MIN * 60
+   short_break_sec = SHORT_BREAK_MIN * 60
+   long_break_sec = LONG_BREAK_MIN * 60
+
+
+   count_down(work_sec)
+   if reps % 8 == 0:
+      count_down(long_break_sec)
+      title_label.config(text="Long Break", fg=RED)
+
+   elif reps % 2 == 0:
+      count_down(short_break_sec)
+      title_label.config(text="Break", fg=PINK)
+   else:
+      count_down(work_sec)
+      title_label.config(text="Work", fg=GREEN)
+
    count_down(5 * 60)
 
 
@@ -25,10 +54,24 @@ def count_down(count):
 
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10:
+       count_sec = f"0{count_sec}"
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-     window.after(1000, count_down, count - 1)
+       global timer
+       timer = window.after(1000, count_down, count - 1)
+
+    else:
+       start_timer()
+       marks = ""
+       work_sessions = math.floor(reps/2)
+       for _ in range(work_sessions):
+          marks += "✔"
+       check_marks.config(text=marks)
+          
+          
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -36,7 +79,6 @@ window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
 title_label = Label(text="Timer", fg="GREEN", bg=YELLOW, font=("FONT_NAME", 50, ))
-
 title_label.grid(column=1, row=0)
 
 tomato_img = PhotoImage(file="tomato.png")
@@ -48,7 +90,7 @@ canvas.grid(column=1, row=1)
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
