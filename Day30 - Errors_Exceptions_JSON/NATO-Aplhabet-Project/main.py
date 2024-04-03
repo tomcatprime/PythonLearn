7c0f23a6f3e6ad647adcc8bbdda1f36dfd84b4b6
tomcatprime <karolchru@gmail.com>
2024-04-03 18:27:47 +0300

    Add JSON data and handling errors

---
+++ Day30 - Errors_Exceptions_JSON/NATO-Aplhabet-Project/main.py
@@ -0,0 +1,40 @@
+#adding error handling
+
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"], 
+#     "score": [56, 76, 98]
+# }
+
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
+
+import pandas as pd
+data = pd.read_csv('./nato_phonetic_alphabet.csv')
+
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     print(row.letter)
+#     #Access row.student or row.score
+#     pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+#TODO 1. Create a dictionary in this format:
+dict = {row.letter:row.code for (index,row) in data.iterrows()}
+print(dict)
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs
+def generate():
+    word = input("Enter a word: ").upper()
+    try:
+        data = [dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet are allowed")
+        generate()
+    else:
+        print(data)
+generate()
