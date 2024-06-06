3aaa5b4cc8237b82ff3a23ef41adef2f894ef56b
tomcatprime <karolchru@gmail.com>
2024-06-06 16:24:45 +0300

    Day 35, Open weather API call

--- Day35 - Api Weather/main.py
+++ Day35 - Api Weather/main.py
@@ -1,1 +1,18 @@
 # First commit after vacation, back to learning
+
+import requests
+
+url = "https://api.openweathermap.org/data/2.5/weather"
+
+api_key = ""
+
+weather_params = {
+    "lat": 44.426765,
+    "lon": 26.102537,
+    "appid": api_key,
+    
+}
+response = requests.get(url, params=weather_params)
+response.raise_for_status()
+
+print(response.json())
