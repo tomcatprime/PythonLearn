714de3c71aed72f7259a37848d47cb7edf07e0aa
tomcatprime <admin@cloudpoweredlearning.com>
2024-06-09 12:42:03 +0300

    commit day35

--- Day35 - Api Weather/main.py
+++ Day35 - Api Weather/main.py
@@ -1,18 +1,22 @@
 # First commit after vacation, back to learning
-
+import json
 import requests
 
-url = "https://api.openweathermap.org/data/2.5/weather"
-
+# Used API from https://www.weatherapi.com/
 api_key = ""
 
+api_open = ""
+
+url_open = "https://api.openweathermap.org/data/2.5/forecast?" #openweather api
+url_weatherapi = "https://api.weatherapi.com/v1//forecast.json" #weatherapi.com
 weather_params = {
     "lat": 44.426765,
     "lon": 26.102537,
-    "appid": api_key,
-    
+    "appid": api_open,
+    "cnt": 4,
 }
-response = requests.get(url, params=weather_params)
-response.raise_for_status()
 
-print(response.json())
+response = requests.get(url_open, params=weather_params)
+json_data = response.json()
+# print(json.dumps(json_data, indent=2))
+print(json_data)
