e27eca01239fd494af6a5c29e67fb63edac63d5a
tomcatprime <karolchru@gmail.com>
2024-08-02 11:07:35 +0300

    commit

--- Day37 - Pixela/main.py
+++ Day37 - Pixela/main.py
@@ -1,14 +1,31 @@
+import os
+from dotenv import load_dotenv
+import json
 import requests
+
+#loading \.env file
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+USERNAME = os.getenv('USERNAME')
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
-
+print(API_KEY)
 user_params = {
-    "token":"type your own token",
-    "username": "jjj",
+    "token":API_KEY,
+    "username":USERNAME,
     "agreeTermsOfService":"yes",
     "notMinor":"yes",
 }
 # Create user
 response = requests.post(url=pixela_endpoint, json=user_params)
+response_json = response.json()
+
+is_success = response.json()
+
 print(response.text)
+print("===================")
+print(response_json)
+print("==============")
+print(is_success)
