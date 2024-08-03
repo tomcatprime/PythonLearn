2f40d3033fe98002d6f5910b2b1db7d23bef5a4e
tomcatprime <karolchru@gmail.com>
2024-08-03 09:55:50 +0300

    pixela PUT

--- Day37 - Pixela/main.py
+++ Day37 - Pixela/main.py
@@ -2,13 +2,14 @@ import os
 from dotenv import load_dotenv
 import json
 import requests
+from datetime import datetime
 
 #loading \.env file
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
 USERNAME = os.getenv('USERNAME')
-
+GRAPH = "graph1"
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
@@ -17,21 +18,15 @@ user_params = {
     "agreeTermsOfService":"yes",
     "notMinor":"yes",
 }
-<<<<<<< HEAD
-# Create user
-response = requests.post(url=pixela_endpoint, json=user_params, verify=False)
-response_json = response.json()
-=======
 # # Create user
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # response_json = response.json()
 # print(response_json)
->>>>>>> 8a88bb2b01621a08d2dc2865fd88cdb945551219
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH,
     "name": "Study Python",
     "unit": "Commit",
     "type": "int",
@@ -44,3 +39,29 @@ headers = {
 response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 
 print(response.text)
+
+# update graph
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH}"
+
+today = datetime.now()
+
+update_config = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "1"
+}
+
+update = requests.post(url=update_endpoint, json=update_config, headers=headers)
+
+print(update.text)
+
+# PUT
+data_to_edit = today.strftime("%Y%m%d")
+edit_endpoint = f"{update_endpoint}/{data_to_edit}"
+
+edit_params = {
+    "quantity": "4"
+}
+
+edit = requests.put(url=update_endpoint, json=edit_params, headers=headers)
+print(edit.text)
