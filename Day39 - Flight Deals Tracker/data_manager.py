5e95a87d1107c86285bcd270549f9e6dee5385da
tomcatprime <karolchru@gmail.com>
2024-08-08 09:49:10 +0300

    change code to OOP in data_manager

--- Day39 - Flight Deals Tracker/data_manager.py
+++ Day39 - Flight Deals Tracker/data_manager.py
@@ -2,25 +2,31 @@ import os
 from dotenv import load_dotenv
 import requests
 from pprint import pprint
-# class DataManager:
-#     #This class is responsible for talking to the Google Sheet.
-#     pass
 
 load_dotenv()
-SHEETY_TOKEN = os.getenv('SHEETY_TOKEN')
 BEARER_TOKEN = os.getenv('BEARER_TOKEN')
-
+SHEETY_TOKEN = os.getenv('SHEETY_TOKEN')
 sheety_endpoint = f"https://api.sheety.co/{SHEETY_TOKEN}/flightDeals/prices"
 
-sheety_headers = sheety_headers = {
+sheety_headers = {
     "Content-Type": "application/json",
     "Authorization": f"Bearer {BEARER_TOKEN}"
 }
 
-# sheety_params = {
+class DataManager:
     
-# }
+    def __init__(self):
+        self.destination_data = {}
+        
+    
+    def get_destination_data(self):
+        response = requests.get(url=sheety_endpoint, headers=sheety_headers)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
 
-sheety_read_data = requests.get(url=sheety_endpoint, headers=sheety_headers, verify=False)
-sheety_read_data.raise_for_status()
-pprint(sheety_read_data.json())
+
+
+
+
+
