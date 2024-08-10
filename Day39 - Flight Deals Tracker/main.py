ccd4333adc79b46c2a11e0662411c135be79881a
tomcatprime <karolchru@gmail.com>
2024-08-10 13:44:13 +0300

    add code to main.py

--- Day39 - Flight Deals Tracker/main.py
+++ Day39 - Flight Deals Tracker/main.py
@@ -1,3 +1,5 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 
-
+import time
+from datetime import date, timedelta
+from data_manager import DataManager
