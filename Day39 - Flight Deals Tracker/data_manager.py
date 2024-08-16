e0bd086ffc9cd7d652c3951e1299044f23be9e25
tomcatprime <karolchru@gmail.com>
2024-08-16 11:22:57 +0300

    Flight Deals Tracker

--- Day39 - Flight Deals Tracker/data_manager.py
+++ Day39 - Flight Deals Tracker/data_manager.py
@@ -20,12 +20,16 @@ class DataManager:
         
     
     def get_destination_data(self):
+        # Use the Sheety API to GET all the data in that sheet and print it out.
         response = requests.get(url=sheety_endpoint, headers=sheety_headers)
         data = response.json()
         self.destination_data = data["prices"]
+         # 3. Try importing pretty print and printing the data out again using pprint() to see it formatted.
+        # pprint(data)
         return self.destination_data
 
-
+     # In the DataManager Class make a PUT request and use the row id from sheet_data
+    # to update the Google Sheet with the IATA codes. (Do this using code).
     def update_destination_codes(self):
         for city in self.destination_data:
             new_data = {
