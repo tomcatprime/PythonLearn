9254a13368cc3bc4537b9546b3f0ea5f3c054ce9
tomcatprime <admin@cloudpoweredlearning.com>
2024-09-01 17:50:52 +0300

    starting code

---
+++ Day46-Spotifyplaylist/main.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+print("Welcome to the Spotify Playlist Generator")
+user_date = input("Enter the date you want to travel to in the format YYYY-MM-DD: ")
+
+print("Thank you. Processing your request...")
+URL = f"https://www.billboard.com/charts/hot-100/{user_date}/"
+
+website = requests.get(URL)
+
+soup = BeautifulSoup(website.text, "lxml")
+song_names_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_names_spans]
+print(song_names)
