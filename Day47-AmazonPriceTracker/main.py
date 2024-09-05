706fa1596faaf4b4e2a7311a065326b622527311
admin@cloudpoweredlearning.com <karolchru@gmail.com>
2024-09-05 17:22:18 +0300

    Amazon price tracker

--- Day47-AmazonPriceTracker/main.py
+++ Day47-AmazonPriceTracker/main.py
@@ -4,12 +4,27 @@ from bs4 import BeautifulSoup
 
 print("Welcome to Amazon Price tracker")
 
-URL = "https://www.amazon.pl/Panasonic-eneloop-akumulatory-opakowaniu-sztucznych/dp/B09ZL9SSD5/ref=sr_1_9?_encoding=UTF8&content-id=amzn1.sym.1efa4f16-e6ed-4dba-96c6-2f14cba2ad54&dib=eyJ2IjoiMSJ9.GtKThdFv8U7fLvQwAQSZtwAHY3GFBf4T9uqAO9X7AWW7n0H9zIqBTosyQ5-rS_3733FGUF0Wpd5Tx26czokk3H-PM1xxpZkFYMuKt3SVEVtxyWVqDuAP5ivCsrJUfgY1u-ZKOfuX3LyZrHUdMMoqL2LBjREj42TLPB1L_-uZBg54sUUre993H_uDtMxXWVn7iIC7uMHSUyKxNiqPX1PCCJyGkwHZLr-r8VP9FaYzIsYjL2NblYPyjKuVBTOqmhe1ZZ9EyjxHfb06WhWgQSXpm2Ot-KNcPFuglIpWJ0xxZAY.pdLcmdQgLwlbEXwFhEdAi2yBLTAd2zOa_APoU67JMq4&dib_tag=se&pd_rd_r=e939c0cc-92db-44e3-b354-6e83771fe9e7&pd_rd_w=QvUdt&pd_rd_wg=yfjwf&pf_rd_p=1efa4f16-e6ed-4dba-96c6-2f14cba2ad54&pf_rd_r=DCVG9YP82EWPB04PZF5Q&qid=1725523400&s=electronics&sr=1-9"
+normal_price = float(input("Enter the normal price of the product: "))
+URL = input("Enter the URL of the product: ")
 
 website = requests.get(URL)
 
 soup = BeautifulSoup(website.text, "lxml")
 
-product_name = soup.find("span", id="productTitle")
+product_name = soup.find("span", id="productTitle").getText().strip()
+last_price = ""
+price_whole = soup.find("span", class_="a-price-whole").getText().strip(",")
+print(price_whole)
+price_fraction = soup.find("span", class_="a-price-fraction").getText()
+print(price_fraction)
+price_symbol = soup.find("span", class_="a-price-symbol").getText()
+print(price_symbol)
+full_price = float(f"{price_whole}.{price_fraction}")
 
-print(product_name)
+if full_price < normal_price:
+    print(f"The price of {product_name} has dropped to {full_price}{price_symbol}.")
+    print("Buy it now!")
+else:
+    print(f"The price of {product_name} is still {full_price}{price_symbol}.")
+    print("No deals available")
+
