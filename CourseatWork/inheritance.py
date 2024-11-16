f5e4772187ad52dbb5bebd30acb30826d5b61915
tomcat <admin@cloudpoweredlearning.com>
2024-11-16 20:44:57 +0200

    Inheritance OOP - course from work

--- CourseatWork/inheritance.py
+++ CourseatWork/inheritance.py
@@ -22,14 +22,27 @@ class Phone:
 class FeaturePhone(Phone):
     pass
 
+# When the child has a method with the same name as that of the parent, it is said be overridden on the parent’s method. T
+# This is called as Method Overriding. Method overriding is also called as Polymorphism.
 class SmartPhone(Phone):
+    def __init__(self, price, brand, camera, os, ram):
+        # To access the parent class constructor  super() can be used.
+        super().__init__(price, brand, camera)
+        self.os = os
+        self.ram = ram
+        print("Inside SmartPhone constructor")
     def check(self):
         print(self.get_price())
-
+    def buy(self):
+        print("Buying a smartphone.")
+        super().buy()
+    def get_os(self):
+        print(self.os)
 FeaturePhone(1000,"Apple", "64px").buy()
 
 # Since the SmartPhone class is inheriting the Phone class, the SmartPhone class inherits the constructor of the Phone class.
-s1=SmartPhone(2000, "Samsung", "108px")
+s1=SmartPhone(2000, "Android", "48px", "Android", 16)
+print(s1.os)
+print(s1.ram)
 s1.buy()
-s1.return_phone()
-s1.check()
+s1.get_os()
