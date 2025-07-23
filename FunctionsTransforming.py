389163ef6d9dc18aefd16a1d6a0b9dd34ec78e1e
tomcatprime <101501305+tomcatprime@users.noreply.github.com>
2025-07-23 11:33:49 +0300

    Update FunctionsTransforming.py

--- FunctionsTransforming.py
+++ FunctionsTransforming.py
@@ -82,3 +82,40 @@ def main():
 
 
 main()
+
+
+Assignment
+Complete the doc_format_checker_and_converter function.
+
+It takes a conversion_function and a list of valid_formats as parameters. It should return a new function that takes two parameters of its own:
+
+filename: The name of the file to be converted
+content: The content (body text) of the file to be converted
+If the file extension of the filename is in the valid_formats list, then it should return the result of calling the conversion_function on the content.
+Otherwise, it should raise a ValueError with the message invalid file format.
+Tip
+I used the .split() method on the filename to get the file extension. You can use the in keyword to check if a value is in a list.
+
+The capitalize_content and reverse_content are "conversion functions" that will be passed into our doc_format_checker_and_converter function by the tests.
+
+def doc_format_checker_and_converter(conversion_function, valid_formats):
+    def inner_func(filename, content):
+        new_file = filename.split('.')[-1]
+        if new_file in valid_formats:
+            return conversion_function(content)
+        else:
+            raise ValueError('invalid file format')
+    return inner_func
+        
+
+
+# Don't edit below this line
+
+
+def capitalize_content(content):
+    return content.upper()
+
+
+def reverse_content(content):
+    return content[::-1]
+
